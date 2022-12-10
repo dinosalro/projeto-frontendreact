5ae7b291ae9847f2056dee12ee321263e26d5eb8
dinosalro <acdclara@gmail.com>
2022-12-09 23:17:05 -0300

    Limpando os inputs

--- src/componentes/Main/Filtros/Filtros.js
+++ src/componentes/Main/Filtros/Filtros.js
@@ -22,9 +22,13 @@ const Filtros = (props) => {
     props.setPrecos({min: -Infinity, max: Infinity })
     props.setLinhas([{bool: false, nome: 'Premium'}, {bool: false, nome: 'Bagaceira'}, {bool: false, nome: 'Junina'}])
     props.setOrdenar("-")
-    
-
-  }
+    setMax("")
+    setMin("")
+    setBagaceira(false)
+    setJunina(false)
+    setPremium(false)
+    props.ordenar("-")
+    }
 
 
   return (
@@ -33,21 +37,21 @@ const Filtros = (props) => {
     
       <div className='checkContainer'>
         <p>LINHA</p>
-        <div className='checkbox'><input  onChange={(e) => {setPremium(e.target.checked)}}  value='Premium' type='checkbox' id='pre'/> <label className="linha1" htmlFor='pre' >PREMIUM</label></div> 
-        <div className='checkbox'><input  onChange={(e) => {setBagaceira(e.target.checked)}}  value="Bagaceira" type='checkbox' id='peb'/> <label className="linha2" htmlFor='peb' >BAGACEIRA</label></div>
-        <div className='checkbox'><input  onChange={(e) => {setJunina(e.target.checked)}}  value="Junina" type='checkbox' id='jun'/> <label className="linha3" htmlFor='jun' >JUNINA</label></div>
+        <div className='checkbox'><input checked={premium} onChange={(e) => {setPremium(e.target.checked)}}  value='Premium' type='checkbox' id='pre'/> <label className="linha1" htmlFor='pre' >PREMIUM</label></div> 
+        <div className='checkbox'><input checked={junina} onChange={(e) => {setBagaceira(e.target.checked)}}  value="Bagaceira" type='checkbox' id='peb'/> <label className="linha2" htmlFor='peb' >BAGACEIRA</label></div>
+        <div className='checkbox'><input checked={bagaceira} onChange={(e) => {setJunina(e.target.checked)}}  value="Junina" type='checkbox' id='jun'/> <label className="linha3" htmlFor='jun' >JUNINA</label></div>
       </div>
       <div>
         <p>ORDENAR</p>
-        <select className='select' onChange={(e) => {props.setOrdenar(e.target.value)}}>
+        <select className='select' value={props.ordenar} onChange={(e) => {props.setOrdenar(e.target.value)}}>
           <option value="-">-</option>
           <option value="alfa">Ordem Alfabetica</option>
         </select>
       </div>
       <div>
         <p>VALOR</p>
-      <input placeholder='Min' type='number' onChange={(e) =>{setMin(Number(e.target.value))}} />
-      <input placeholder='Max' type='number' onChange={(e) =>{setMax(Number(e.target.value))}}/>
+      <input value={min} placeholder='Min' type='number' onChange={(e) =>{setMin(Number(e.target.value))}} />
+      <input value={max} placeholder='Max' type='number' onChange={(e) =>{setMax(Number(e.target.value))}}/>
       </div>
       <button className='botaoFiltros' onClick={addFiltros}>Filtrar</button>
       <button className='botaoRemover' onClick={removerFiltros}>Remover</button>
